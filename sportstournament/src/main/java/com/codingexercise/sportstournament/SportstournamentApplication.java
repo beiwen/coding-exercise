4b65269c7cb046aa0df2edda121cacb0570ceb3c
beiwen <guo.bei@husky.neu.edu>
2019-01-08 21:49:35 -0500

    setup spring boot application

---
+++ sportstournament/src/main/java/com/codingexercise/sportstournament/SportstournamentApplication.java
@@ -0,0 +1,14 @@
+package com.codingexercise.sportstournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportstournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportstournamentApplication.class, args);
+	}
+
+}
+
